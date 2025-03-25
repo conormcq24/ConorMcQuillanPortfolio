ab6b467c9e7fd90ef2b15a31c00dd1b07c444e4f
conormcq24 <conormcq24@gmail.com>
2025-03-25 17:31:23 -0500

    testing testing testing

--- ConorMcQuillanPortfolio/Controllers/HomeController.cs
+++ ConorMcQuillanPortfolio/Controllers/HomeController.cs
@@ -248,7 +248,7 @@ public class HomeController : Controller
         catch (Exception ex)
         {
             _logger.LogError(ex, "Error sending a message: " + ex.Message);
-            TempData["ErrorMessage"] = "Failed to send message: " + ex.Message;
+            TempData["ErrorMessage"] = "Failed to send a message: " + ex.Message;
             return RedirectToAction("Index");
         }
     }
